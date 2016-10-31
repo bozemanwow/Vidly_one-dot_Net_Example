5fbf70d84a5ea6a21f04986f3b83f3b3d335663f
Todd Bozeman <bozemanwow@gmail.com>
2016-10-30 17:09:52 -0700

    Add project files.

---
+++ Vidly_one/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly_one.MvcApplication" Language="C#" %>
---
+++ Vidly_one/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_one.Startup))]
+namespace Vidly_one
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
